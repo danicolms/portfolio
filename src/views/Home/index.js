8b3742fe5110326af80c95d658e720ba1285e3bb
danicolms <danicolms@icloud.com>
2023-07-28 16:47:23 +0200

    :sparkles:  nav & content

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./styles.css";
 
 const Home = () => {
-  return <div>Welcome to the portfolio v2</div>;
+  return (
+    <div className="container">
+      <div className="viewer">
+        <nav className="viewer__item viewer__nav"> Is Nav</nav>
+        <section className="viewer__item viewer__content"> Is Content </section>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
