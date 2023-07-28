ec1b80803a788f534cde6bca9f5fee9ad711b644
danicolms <danicolms@icloud.com>
2023-07-28 15:23:31 +0200

    :fire: remove typescript and styled-components dependencies
    We're gonna use React's proptypes and CSS (maybe Bulma)

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./views/Home";
+
+console.log("💡 Running v2.0.0");
+
+export const App = () => {
+  return (
+    <React.Fragment>
+      <Home />
+    </React.Fragment>
+  );
+};
