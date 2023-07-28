729c159c9d639f2e29453468baac8fa4cfc936e0
danicolms <danicolms@icloud.com>
2023-07-28 22:50:15 +0200

    :recycle: fix copy and shadows on landing

--- src/views/Landing/index.js
+++ src/views/Landing/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 
 import "./styles.css";
+
 const Landing = () => {
   return (
     <Fragment>
@@ -10,12 +11,12 @@ const Landing = () => {
       </p>
       <br />
       <p>
-        As an adept computer engineer, I excel in full-stack development, with a
-        passion for creating visually appealing and user-friendly interfaces. My
-        expertise encompasses a wide range of programming languages, frameworks,
-        and tools, enabling me to deliver seamless and innovative projects, as
-        my commitment to staying updated with the latest industry trends ensures
-        that I continuously bring fresh perspectives to my projects.
+        As an adept computer engineer, I have a passion for creating visually
+        appealing and user-friendly interfaces. My expertise encompasses a wide
+        range of programming languages, frameworks, and tools, enabling me to
+        deliver seamless and innovative projects, as my commitment to staying
+        updated with the latest industry trends ensures that I continuously
+        bring fresh perspectives to my projects.
       </p>
     </Fragment>
   );
