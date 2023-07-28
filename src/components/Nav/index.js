78434ee979b97f0949a7b67797ec5afc207b2017
danicolms <danicolms@icloud.com>
2023-07-28 17:54:32 +0200

    :lipstick: fill nav width with item

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -18,15 +18,15 @@ function Nav({ sections, currentSection, setCurrentSection }) {
 
       <ul className="nav__items">
         {sections.map((section) => (
-          <li
-            key={section.key}
-            className={classNames("nav__item", {
-              ["nav__item--selected"]: section.key === currentSection.key,
-            })}
-          >
-            <button onClick={() => setCurrentSection(section)}>
+          <li key={section.key}>
+            <a
+              className={classNames("nav__item", {
+                ["nav__item--selected"]: section.key === currentSection.key,
+              })}
+              onClick={() => setCurrentSection(section)}
+            >
               {section.value}
-            </button>
+            </a>
           </li>
         ))}
       </ul>
