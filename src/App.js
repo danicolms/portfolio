2f62e928692140cb47a040af4aa65b673c7ee029
danicolms <danicolms@icloud.com>
2023-07-31 14:01:13 +0200

    :bug: CAN I PUSH TO MAIN?

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./views/Home";
 
-console.log("💡 Running v2.0.0");
+console.log("💡 Running v2.0.0", "CAN I PUSH?");
 
 export const App = () => {
   return (
