eb7bba8ec23a55bae00410edf703e50036c471f5
danicolms <danicolms@icloud.com>
2023-07-31 14:01:38 +0200

    :fire: remove comment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./views/Home";
 
-console.log("💡 Running v2.0.0", "CAN I PUSH?");
+console.log("💡 Running v2.0.0");
 
 export const App = () => {
   return (
